6cb688c6a9bfb9eb62580d6262dae3b3e25916ab
Hamza Zerhouni <hamza.zerhouni@ozon.io>
2018-09-28 14:27:23 +0200

    FIX/MINOR: Fix User-Agent parsing

--- wig/wig.py
+++ wig/wig.py
@@ -302,7 +302,7 @@ def parse_args(url=None):
 	parser.add_argument('-m', action='store_true', dest='match_all', default=False,
 		help='Try harder to find a match without making more requests')
 
-	parser.add_argument('-u', action='store_true', dest='user_agent',
+	parser.add_argument('-u', type=str, dest='user_agent',
 		default='Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36',
 		help='User-agent to use in the requests')
 
